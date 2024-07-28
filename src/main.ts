986364c6e4c322c751960835bdaab4044ce887b1
Htet Win Khant <htetwinkhant999@gmail.com>
2024-07-28 10:12:41 +0630

    cors update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:5173', // Replace with your frontend URL
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://dashboad-tasty.vercel.app',
+    ],
     credentials: true, // This allows cookies to be sent from the frontend
   });
   await app.listen(3000);
